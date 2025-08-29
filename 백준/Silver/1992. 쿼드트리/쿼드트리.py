a895e4eb8fb2a54bf4fe7c8b5e900b5fc768cb2d
mango3461 <80249639+mango3461@users.noreply.github.com>
2025-08-29 23:49:52 +0900

    [Silver I] Title: 쿼드트리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,34 +1,20 @@
 import sys
 
-N = int(sys.stdin.readline())
-video = [sys.stdin.readline().strip() for _ in range(N)]
+N = int(input())
+image = [input().strip() for _ in range(N)]
 
-def compress(x, y, size):
-    first_pixel = video[x][y]
-    is_same = True
+def quad_tree(n, y, x):
+    initial = image[y][x]
+    for i in range(y, y + n):
+        for j in range(x, x + n):
+            if image[i][j] != initial:
+                half = n // 2
+                return "(" + \
+                    quad_tree(half, y, x) + \
+                    quad_tree(half, y, x + half) + \
+                    quad_tree(half, y + half, x) + \
+                    quad_tree(half, y + half, x + half) + ")"
+    return initial
 
-    # 현재 영역이 모두 같은 색인지 확인
-    for i in range(x, x + size):
-        for j in range(y, y + size):
-            if video[i][j] != first_pixel:
-                is_same = False
-                break
-        if not is_same:
-            break
+print(quad_tree(N, 0, 0))
 
-    # 모두 같은 색이라면 해당 색상 출력
-    if is_same:
-        print(first_pixel, end='')
-        return
-
-    # 다른 색이 섞여 있다면 4등분하여 재귀 호출
-    half = size // 2
-    print('(', end='')
-    compress(x, y, half)             # 왼쪽 위
-    compress(x, y + half, half)      # 오른쪽 위
-    compress(x + half, y, half)      # 왼쪽 아래
-    compress(x + half, y + half, half) # 오른쪽 아래
-    print(')', end='')
-
-# 함수 호출
-compress(0, 0, N)
